a2e114605ebb45bdc47900e6edc39cc84b5adfb7
JeanBF-USJ <jeanbechara.francis@net.usj.edu.lb>
2024-02-25 22:02:21 +0200

    Added more customization to APIManager + auto logout + user profile endpoint called on Hub enter

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -2,24 +2,36 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class APIManager : MonoBehaviour
 {
     private string _baseUrl = "http://localhost:3000";
 
-    public delegate void AuthCallback(UnityWebRequest response);
+    public delegate void ResponseCallback(UnityWebRequest response);
     
-    public IEnumerator SendRequest(string apiEndpoint, string jsonStr, AuthCallback callback)
+    public IEnumerator SendRequest(string apiEndpoint, string token, string jsonStr, ResponseCallback callback)
     {
         using (UnityWebRequest www = UnityWebRequest.PostWwwForm(_baseUrl + apiEndpoint, "POST"))
         {
+            www.SetRequestHeader("Content-Type", "application/json");
+            if (token != null) www.SetRequestHeader("Authorization", token);
+
+            if (jsonStr == null) jsonStr = "{}";
             byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonStr);
             www.uploadHandler = new UploadHandlerRaw(bodyRaw);
-            www.SetRequestHeader("Content-Type", "application/json");
 
             yield return www.SendWebRequest();
 
-            callback(www);
+            if (www.responseCode == 401) Logout();
+            else callback(www);
         }
+    }
+
+    public void Logout()
+    {
+        PlayerPrefs.DeleteKey("token");
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/HubManager.cs
+++ Assets/Scripts/HubManager.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class HubManager : MonoBehaviour
@@ -18,16 +19,37 @@ public class HubManager : MonoBehaviour
     private void Start()
     {
         _apiManager = GetComponent<APIManager>();
+        
         string savedToken = PlayerPrefs.GetString("token");
-        if (!string.IsNullOrEmpty(savedToken))
+        if (string.IsNullOrEmpty(savedToken)) _apiManager.Logout();
+        else
         {
-            coinsText.text = "1000";
+            string apiEndpoint = "/user/profile";
+            StartCoroutine(_apiManager.SendRequest(apiEndpoint, savedToken, null, HandleResponse));
         }
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(2);
+    }
+
+    private void HandleResponse(UnityWebRequest www)
+    {
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            UserProfileResponse response = JsonUtility.FromJson<UserProfileResponse>(www.downloadHandler.text);
+            Debug.Log("Email: " + response.email);
+            Debug.Log("Username: " + response.username);
+            Debug.Log("Birthdate: " + response.birthdate);
+            Debug.Log("Coins: " + response.coins);
+            foreach (string accessory in response.accessories)
+            {
+                Debug.Log("Accessory: " + accessory);
+            }
+
+            coinsText.text = response.coins.ToString();
+        }
     }
 
     public void GoToLobby()
@@ -52,4 +74,14 @@ public class HubManager : MonoBehaviour
         if (lobbyScreen.activeInHierarchy) lobbyScreen.SetActive(false);
         if (lockerScreen.activeInHierarchy) lockerScreen.SetActive(false);
     }
+}
+
+[Serializable]
+public class UserProfileResponse
+{
+    public string email;
+    public string username;
+    public string birthdate;
+    public int coins;
+    public string[] accessories;
 }
